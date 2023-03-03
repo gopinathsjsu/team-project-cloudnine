8c947fab7127f0ba586a7855c1715536b2d1c211
Rahul <rahul.pillai03@gmail.com>
2023-03-03 10:45:34 -0800

    Added repository

---
+++ backend/src/main/java/com/example/carb_crusher/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.carb_crusher;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
+    List<User> findByGymLocationId(String gymLocationId);
+}
+
